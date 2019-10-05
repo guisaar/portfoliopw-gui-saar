47bb7160602ba99653816f83a17ee7c8afeeed96
guisaar <56200519+guisaar@users.noreply.github.com>
2019-10-05 14:07:47 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,7 +1,7 @@
 <?php
-define('HOST', '127.0.0.1');
-define('USUARIO', 'root');
-define('SENHA', 'canaltiyoutube');
-define('DB', 'login');
+define('HOST', 'us-cdbr-iron-east-05.cleardb.net');
+define('USUARIO', 'bc6a8c1fb3888a');
+define('SENHA', 'ca80bcd0');
+define('DB', 'heroku_28b1ceed182c821');
 
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
