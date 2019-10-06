0486c5dcaaf00074faf1ee29c327e23e67f4d265
guisaar <56200519+guisaar@users.noreply.github.com>
2019-10-06 12:55:33 -0300

    Update painel.php

--- painel.php
+++ painel.php
@@ -4,4 +4,5 @@ include('verifica_login.php');
 ?>
 
 <h2>Olá, <?php echo $_SESSION['usuario'];?></h2>
+<h2> <?php echo $_SESSION['usuario'];?>Tudo bem,queres configurauma melhor experiencia?</h2>
 <h2><a href="logout.php">Sair</a></h2>
