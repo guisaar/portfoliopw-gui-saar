69281ed7837ab475a624b14a0a66f2ba64c686ce
guisaar <56200519+guisaar@users.noreply.github.com>
2019-11-09 14:38:49 -0300

    Delete conexao.php

--- conexao.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('HOST', 'us-cdbr-iron-east-05.cleardb.net');
-define('USUARIO', 'bc6a8c1fb3888a');
-define('SENHA', 'ca80bcd0');
-define('DB', 'heroku_28b1ceed182c821');
-
-$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
